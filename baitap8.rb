c026155a58bc40b5569593ab61312b081be4291b
VuVT <truongvuuit@gmail.com>
2021-12-04 10:49:51 +0700

    Review ex8

--- baitap8.rb
+++ baitap8.rb
@@ -8,3 +8,6 @@ end
 array_one= [-2,3,6,8,1,14,7,5]
 array_two= [-2,1,7,5]
 puts " #{check_sub_arrays(array_one,array_two)}\n"
+
+# Code e viết rất gọn. Tuy nhiên, e sử dụng include? thì cũng giống như sử dụng vòng for thứ 2 ah e, vì bản chất, include? cũng sẽ duyệt qua từng
+# phần tử trong mảng để check ah
